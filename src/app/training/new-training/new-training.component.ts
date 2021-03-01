daf578da8dffd7c287af046f6a7383976b3196b1
John Biddulph <johnbiddulph@Johns-MacBook-Pro.local>
2021-03-01 09:12:46 +0000

    new training adjustments

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -2,9 +2,9 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Exercise } from 'src/app/models/exercise.model';
 import { TrainingService } from '../services/training.service';
-import { AngularFirestore } from '@angular/fire/firestore';
+
 import { Observable, Subscription } from 'rxjs';
-import { map } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-new-training',
@@ -15,7 +15,7 @@ export class NewTrainingComponent implements OnInit, OnDestroy {
   exercises: Exercise[];
   exerciseSubscription : Subscription;
 
-  constructor(private trainingService: TrainingService, private db: AngularFirestore) { }
+  constructor(private trainingService: TrainingService) { }
 
   ngOnInit(): void {
     this.exerciseSubscription = this.trainingService.exercisesChanged.subscribe(exercises => { this.exercises = exercises})
